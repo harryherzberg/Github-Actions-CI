b937b786fbfeb75d079506a9821af8debde293fd
harryherzberg <harryherzberg@gmail.com>
2021-06-05 15:54:02 -0700

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,11 @@ class TestCase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(1, 1), 1)
+    
+    def test_divide_1(self):
+        self.assertEqual(example.devide(1, 1), 1)
 
 if __name__ == '__main__':
     unittest.main()
